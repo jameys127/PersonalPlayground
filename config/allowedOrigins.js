5e1cc73c3b14a9dfc53a2094d9d90b0495b1d2e0
Jeremiah Sheehy <jameys127@gmail.com>
2025-09-07 11:22:35 -0700

    backend basics are up with logging and 3rd party middleware. Time to get PostGreSQL

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3500',
+    'https://www.rustypersonalplayground.com',
+    'https://rustypersonalplayground.com'
+]
+
+module.exports = allowedOrigins;
